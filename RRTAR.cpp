1ff236be3e4742f6edf56020e55eb729978e23ae
Matthew <matthew0517@gmail.com>
2023-06-20 16:52:58 -0400

    Added tree plot

--- RRTAR.cpp
+++ RRTAR.cpp
@@ -13,14 +13,14 @@ using namespace std;
 
 struct Node {
     vector<float> cord;
-    Node* parent;
+    int parent;
     int pos;
     Node() {
         cord = {0.0,0.0};
-        parent = NULL;
-        pos = -1;
+        parent = -1;
+        pos = -100;
     }
-    Node(float xOrigin, float yOrigin, Node* parental, int posIn) {
+    Node(float xOrigin, float yOrigin, int parental, int posIn) {
         cord = {xOrigin,yOrigin};
         parent = parental;
         pos = posIn;
@@ -51,7 +51,7 @@ Node steer(float x, float y, float radius, Node node, int count) {
 
     if (distance <= radius) {
         // If the distance is within the radius, create a new node at the given coordinates without projection
-        Node newNode = Node(x, y, &node, count);
+        Node newNode = Node(x, y, node.pos, count);
         return newNode;
     } else {
         // Calculate the angle between the current node's coordinates and the projected coordinates
@@ -62,18 +62,18 @@ Node steer(float x, float y, float radius, Node node, int count) {
         float projectedY = node.cord[1] + radius * sin(angle);
 
         // Create a new node with the projected coordinates and set its parent to the input node
-        Node projectedNode = Node(projectedX, projectedY, &node, count);
+        Node projectedNode = Node(projectedX, projectedY, node.pos, count);
         return projectedNode;
     }
 }
 
-vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, float rad) {
+vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, float rad, vector<vector<int>> og) {
 
     vector<Node> searchTree;
-    Node origin = Node(initialX, initialY, NULL, 0);
+    Node origin = Node(initialX, initialY, -1, 0);
     searchTree.push_back(origin);
     vector<vector<float>> sol;
-    int count = 0;
+    int count = 1;
 
     while (count < maxNodesIn) {
         //std::srand(std::time(nullptr));
@@ -81,14 +81,27 @@ vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, flo
         // Generate two random floats between 0 and 100
         float x = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * 100.0f;
         float y = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * 100.0f;
+        if (og[static_cast<int>(x)][static_cast<int>(y)]) {
+            continue;
+        }
         Node close = closest(searchTree, x, y);
+
         Node Xnew = steer(x,y,rad,close, count);
+        if (og[static_cast<int>(Xnew.cord[0])][static_cast<int>(Xnew.cord[1])]) {
+            continue;
+        }
         searchTree.push_back(Xnew);
         count++;
     }
 
     for (Node nodes : searchTree) {
-        sol.push_back(nodes.cord);
+        vector<float> temp = nodes.cord;
+        if (nodes.parent >= 0) {
+            temp.push_back(static_cast<float>(nodes.parent));
+        } else {
+            temp.push_back(-1.0f);
+        }
+        sol.push_back(temp);
     }
     return sol;
 }
