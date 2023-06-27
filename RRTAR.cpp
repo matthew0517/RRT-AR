71fa4d7c15f1487befc2b985121f2a0465af55bf
Matthew <matthew0517@gmail.com>
2023-06-27 13:54:06 -0400

    Working RRT*

--- RRTAR.cpp
+++ RRTAR.cpp
@@ -10,20 +10,39 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+vector<vector<float>> goals;
+float rad;
+// Function to check if a point is within r of the goal. 
+int checkInGoal(float x, float y) {
+    for (int i =0; (unsigned) i < goals.size(); i++) {
+        vector<float> elem = goals[i];
+        float dist = (elem[0] - x)*(elem[0] - x) + (elem[1] - y)*(elem[1] - y);
+        if (dist < rad) {
+            return i+1;
+        }
+    }
+    return 0;
+}
 
 struct Node {
     vector<float> cord;
     int parent;
     int pos;
+    int inGoal;
+    float cost;
     Node() {
         cord = {0.0,0.0};
         parent = -1;
         pos = -100;
+        inGoal = 0;
+        cost = 0.0;
     }
-    Node(float xOrigin, float yOrigin, int parental, int posIn) {
+    Node(float xOrigin, float yOrigin, int parental, int posIn, int inGoalIn, float costIn) {
         cord = {xOrigin,yOrigin};
         parent = parental;
         pos = posIn;
+        inGoal = inGoalIn;
+        cost = costIn;
     }
 };
 
@@ -31,12 +50,12 @@ int add(int i, int j) {
     return i + j;
 }
 
-Node closest(vector<Node> searchTree, float x, float y) {
+Node closest(vector<Node> const &searchTree, float x, float y) {
     float minDist = numeric_limits<float>::max();
     Node sol;
     for (Node elem : searchTree) {
         float dist = (elem.cord[0] - x)*(elem.cord[0] - x) + (elem.cord[1] - y)*(elem.cord[1] - y);
-        if (dist < minDist) {
+        if (dist < minDist && !elem.inGoal) {
             minDist = dist;
             sol = elem;
         }
@@ -45,34 +64,80 @@ Node closest(vector<Node> searchTree, float x, float y) {
 }
 
 // Author:ChatGPT
-Node steer(float x, float y, float radius, Node node, int count) {
+Node steer(float x, float y, Node node, int count) {
     // Calculate the distance between the current node's coordinates and the given coordinates
     float distance = sqrt(pow(x - node.cord[0], 2) + pow(y - node.cord[1], 2));
 
-    if (distance <= radius) {
+    if (distance <= rad) {
         // If the distance is within the radius, create a new node at the given coordinates without projection
-        Node newNode = Node(x, y, node.pos, count);
+        Node newNode = Node(x, y, node.pos, count, checkInGoal(x, y), distance);
         return newNode;
     } else {
         // Calculate the angle between the current node's coordinates and the projected coordinates
         float angle = atan2(y - node.cord[1], x - node.cord[0]);
 
         // Calculate the projected x and y coordinates on the circle
-        float projectedX = node.cord[0] + radius * cos(angle);
-        float projectedY = node.cord[1] + radius * sin(angle);
+        float projectedX = node.cord[0] + rad * cos(angle);
+        float projectedY = node.cord[1] + rad * sin(angle);
 
         // Create a new node with the projected coordinates and set its parent to the input node
-        Node projectedNode = Node(projectedX, projectedY, node.pos, count);
+        Node projectedNode = Node(projectedX, projectedY, node.pos, count, checkInGoal(projectedX, projectedY), rad);
         return projectedNode;
     }
 }
 
-vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, float rad, vector<vector<int>> og) {
+float calcCost(Node input, vector<Node> const &searchTree) {
+    float ans = input.cost;
+    while (input.parent > -1) {
+        input = searchTree[input.parent];
+        ans += input.cost;
+    }
+    return ans;
+}
 
+void rewire(vector<Node> &searchTree, int count) {
+    vector<int> withRad;
+    int minNeighbor = -1;
+    float minCost =  numeric_limits<float>::max();
+    vector<float> costs;
+    vector<float> dists;
+
+    float x = searchTree[count].cord[0];
+    float y = searchTree[count].cord[1];
+    for (int i = 0; i < count; i++) {
+        float dist = sqrt((searchTree[i].cord[0] - x)*(searchTree[i].cord[0] - x) + (searchTree[i].cord[1] - y)*(searchTree[i].cord[1] - y));
+        if (dist < rad) {
+            withRad.push_back(i);
+            float cost = calcCost(searchTree[i], searchTree);
+            costs.push_back(cost);
+            dists.push_back(dist);
+            if (cost + dist < minCost) {
+                minCost = cost+dist;
+                minNeighbor = i;
+            }
+        }
+    }
+    if (minNeighbor > -1) {
+        searchTree[count].parent = minNeighbor;
+    }
+
+    for (int i = 0; (unsigned) i < costs.size(); i++) {
+        if (minCost + dists[i] < costs[i]) {
+            searchTree[withRad[i]].parent = count;
+        }
+    }
+
+
+
+}
+
+vector<vector<vector<float>>> runRRT(float initialX, float initialY, int maxNodesIn, float radIn, vector<vector<int>> og, vector<vector<float>> goalsIn) {
+    goals = goalsIn;
+    rad = radIn;
     vector<Node> searchTree;
-    Node origin = Node(initialX, initialY, -1, 0);
+    Node origin = Node(initialX, initialY, -1, 0, false, 0);
     searchTree.push_back(origin);
-    vector<vector<float>> sol;
+    vector<vector<vector<float>>> sol;
     int count = 1;
 
     while (count < maxNodesIn) {
@@ -86,14 +151,17 @@ vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, flo
         }
         Node close = closest(searchTree, x, y);
 
-        Node Xnew = steer(x,y,rad,close, count);
+        Node Xnew = steer(x,y,close, count);
         if (og[static_cast<int>(Xnew.cord[0])][static_cast<int>(Xnew.cord[1])]) {
             continue;
         }
         searchTree.push_back(Xnew);
+        rewire(searchTree, count);
         count++;
     }
 
+    // Adds every branch in the vector
+    vector<vector<float>> fulltree;
     for (Node nodes : searchTree) {
         vector<float> temp = nodes.cord;
         if (nodes.parent >= 0) {
@@ -101,12 +169,31 @@ vector<vector<float>> runRRT(float initialX, float initialY, int maxNodesIn, flo
         } else {
             temp.push_back(-1.0f);
         }
-        sol.push_back(temp);
+        fulltree.push_back(temp);
     }
+    sol.push_back(fulltree);
+
+    // Adds branches that connect to the goal
+    for (Node nodes : searchTree) {
+        if (nodes.inGoal > 0) {
+            int parentIndex = nodes.parent;
+            vector<vector<float>> solBranch;
+            vector<float> temp = nodes.cord;
+            solBranch.push_back(goals[nodes.inGoal-1]);
+            solBranch.push_back(temp);
+            while (parentIndex >= 0) {
+                Node nodesInner = searchTree[parentIndex];
+                temp = nodesInner.cord;
+                solBranch.push_back(temp);
+                parentIndex = nodesInner.parent;
+            }
+            reverse(solBranch.begin(), solBranch.end());
+            sol.push_back(solBranch);
+        }
+    }
+    
     return sol;
 }
-
-
 
 PYBIND11_MODULE(RRTAR, handle) {
     handle.doc() = "This is the module docs.";
